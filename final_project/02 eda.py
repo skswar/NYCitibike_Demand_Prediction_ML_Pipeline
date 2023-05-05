7e5a167ea38b5ee664c29cbebd25bb93c7e48240
sharadsin29 <sharadsin29>
2023-05-05 17:50:02 +0000

    final eda commit

--- final_project/02 eda.py
+++ final_project/02 eda.py
@@ -3,17 +3,19 @@
 
 # COMMAND ----------
 
-# start_date = str(dbutils.widgets.get('01.start_date'))
-# end_date = str(dbutils.widgets.get('02.end_date'))
-# hours_to_forecast = int(dbutils.widgets.get('03.hours_to_forecast'))
-# promote_model = bool(True if str(dbutils.widgets.get('04.promote_model')).lower() == 'yes' else False)
-
-# print(start_date,end_date,hours_to_forecast, promote_model)
-# print("YOUR CODE HERE...")
-
-# COMMAND ----------
-
-spark.sql(f"USE {GROUP_DB_NAME}")
+# Import Statements
+from pyspark.sql.functions import date_format
+from pyspark.sql.functions import month, sum
+from pyspark.sql.functions import to_timestamp
+from pyspark.sql.functions import month, year, count
+from pyspark.sql.functions import date_trunc
+import holidays
+from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
+from pyspark.sql.functions import avg
+from pyspark.sql.functions import col,round
+from pyspark.sql.functions import hour,date_trunc
+from pyspark.sql.types import StringType, FloatType
+import pyspark.sql.functions as F
 
 # COMMAND ----------
 
@@ -26,142 +28,8 @@ for file in files:
 
 # COMMAND ----------
 
-merged_df = spark.read.format('delta').option('header', True).option('inferSchema', True).load("dbfs:/FileStore/tables/G07/silverbike_weather_g07")
-display(merged_df)
-
-# COMMAND ----------
-
-# merged_df.createOrReplaceTempView("my_bike_view")
-
-# COMMAND ----------
-
-# df = spark.sql("""
-# select CONCAT(CAST(MONTH(started_at) AS VARCHAR(10)),"-",CAST(YEAR(started_at) AS VARCHAR(10))), count(ride_id) as cnt
-# from my_bike_view
-# group by MONTH(started_at), YEAR(started_at)
-# order by YEAR(started_at),MONTH(started_at)""")
-
-# display(df)
-
-# COMMAND ----------
-
-# %sql
-# SELECT DAY(started_at), COUNT(ride_id) as cnt
-# FROM my_bike_view
-# GROUP BY DAY(started_at)
-
-# COMMAND ----------
-
-# %sql
-# SELECT DATE(started_at) as start_date, COUNT(ride_id) as cnt
-# FROM my_bike_view
-# GROUP BY DATE(started_at)
-
-# COMMAND ----------
-
-# %sql
-# SELECT DATE(started_at) as start_date, COUNT(ride_id) as cnt
-# FROM my_bike_view
-# GROUP BY DATE(started_at)
-
-# COMMAND ----------
-
 # MAGIC %md
-# MAGIC First dip that we see is in November 25th, 2021 because of Thanksgiving.<br>
-# MAGIC Secondly we see dip is in December 25th, 2021 another valid reason as it was Christmas.<br>
-# MAGIC There was a big snowstorm at East Coast, USA on Jan 29th 2022 thus explains the dip at NYC.
-# MAGIC Also the long weekend of President's day on Feb 21st, 2022 explained a dip.Like this there are dips holidays such as Easter (April 17th), Independence Day (July 4th) etc. which are explaing that less number of people used the bike service.
-
-# COMMAND ----------
-
-from pyspark.sql.functions import date_format
-
-# COMMAND ----------
-
-# %sql
-# SELECT date_format(started_at,'EEEE') as start_date, COUNT(ride_id) as cnt
-# FROM my_bike_view
-# GROUP BY date_format(started_at,'EEEE')
-# ORDER BY  DAYOFWEEK(start_date)
-
-# COMMAND ----------
-
-# %sql
-# SELECT HOUR(started_at) as Hour_of_Day, COUNT(ride_id) as cnt
-# FROM my_bike_view
-# GROUP BY HOUR(started_at)
-# ORDER BY HOUR(started_at)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC We saw that there are less number of rides during midnight and early morning (12AM to 6AM).<br>
-# MAGIC There is peak during late noon to evening hours (highest between 4PM to 6PM).<br>
-# MAGIC This pattern explains the normal working schedule of general population.
-
-# COMMAND ----------
-
-# wdf=spark.read.format('csv').option("header","True").option("inferSchema","True").load(NYC_WEATHER_FILE_PATH)
-# display(wdf)
-
-# COMMAND ----------
-
-# from pyspark.sql.functions import date_format, to_timestamp, from_utc_timestamp,from_unixtime,lit
-# wdf.createOrReplaceTempView("my_weather_view")
-
-# COMMAND ----------
-
-# %sql
-# SELECT distinct from_unixtime(dt) as datetime, hour(from_unixtime(dt)) as hour_value, date(from_unixtime(dt)) as date_value ,*
-# FROM my_weather_view
-# order by datetime
-
-# COMMAND ----------
-
-# %sql
-# SELECT tab1.ride_id, from_unixtime(tab2.dt) weather_date,* 
-# FROM my_bike_view tab1
-# INNER JOIN my_weather_view as tab2
-# ON DATE(tab1.started_at) = DATE(from_unixtime(tab2.dt))
-# AND HOUR(tab1.started_at) = HOUR(from_unixtime(tab2.dt))
-# WHERE DATE(from_unixtime(tab2.dt)) BETWEEN '2022-01-28' AND '2022-01-30'
-
-# -- SELECT min(DATE(from_unixtime(dt))) as mindate, max(DATE(from_unixtime(dt))) as maxdate
-# -- FROM my_weather_view
-
-# COMMAND ----------
-
-# %sql
-# SELECT tab2.main as weathercondtion, COUNT(tab1.ride_id) as ridecount
-# FROM my_bike_view as tab1
-# INNER JOIN my_weather_view as tab2
-# ON DATE(tab1.started_at) = DATE(from_unixtime(tab2.dt))
-# AND HOUR(tab1.started_at) = HOUR(from_unixtime(tab2.dt))
-# GROUP BY tab2.main
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-display(merged_df)
-
-# COMMAND ----------
-
-from pyspark.sql.functions import month, sum
-
-monthly_trip_trends = merged_df.groupBy(month("parsed_dt").alias("month")) \
-    .agg(sum("net_trip_difference").alias("total_trips")) \
-    .orderBy("month")
-
-display(monthly_trip_trends)
-
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Reading in Bronze Data
+# MAGIC Reading in Ride Data
 
 # COMMAND ----------
 
@@ -170,15 +38,11 @@ display(bronze_df)
 
 # COMMAND ----------
 
-from pyspark.sql.functions import to_timestamp
-
 bronze_df = bronze_df.withColumn("started_at", to_timestamp("started_at", "yyyy-MM-dd HH:mm:ss")) \
     .withColumn("ended_at", to_timestamp("ended_at", "yyyy-MM-dd HH:mm:ss"))
 
 
 # COMMAND ----------
-
-from pyspark.sql.functions import month, year, count
 
 assigned_station_id = 6173.08
 monthly_trips = bronze_df.filter(bronze_df.start_station_id == assigned_station_id) \
@@ -191,7 +55,10 @@ display(monthly_trips)
 
 # COMMAND ----------
 
-from pyspark.sql.functions import date_trunc
+# MAGIC %md
+# MAGIC As the summer months approach, we observe an increase in the number of rides. However, there is a slight dip in June as temperatures become excessively hot. As fall arrives, the weather becomes more favorable for bike rides, leading to a rise in the number of rides once again. When winter and colder months set in, accompanied by snow, the number of rides experiences a sharp decline, which persists until the arrival of spring.
+
+# COMMAND ----------
 
 daily_trips = bronze_df.filter(bronze_df.start_station_id == assigned_station_id) \
     .groupBy(date_trunc("day", "started_at").alias("day")) \
@@ -203,9 +70,35 @@ display(daily_trips)
 
 # COMMAND ----------
 
-import holidays
-from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
-from pyspark.sql.functions import avg
+# MAGIC %md
+# MAGIC The first noticeable dip in usage occurred on November 25th, 2021, which can be attributed to the Thanksgiving holiday. The next dip took place on December 25th, 2021, a valid reason being that it was Christmas Day. On January 29th, 2022, a significant snowstorm hit the East Coast of the USA, explaining the decrease in bike usage in New York City. Additionally, the long weekend for President's Day on February 21st, 2022 also accounted for a dip in usage. Similarly, there are dips in usage during other holidays, such as Easter (April 17th) and Independence Day (July 4th), which demonstrate that fewer people utilize the bike service on these occasions.
+
+# COMMAND ----------
+
+bronze_df.createOrReplaceTempView("my_bike_view")
+df_year_month = spark.sql("""
+select CONCAT(CAST(YEAR(started_at) AS VARCHAR(10)) ,"-", LPAD(MONTH(started_at), 2, '0')) as YearMonth, count(ride_id) as cnt
+from my_bike_view
+group by MONTH(started_at), YEAR(started_at)
+order by YEAR(started_at),MONTH(started_at)""")
+display(df_year_month)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT date_format(started_at,'EEEE') as start_date, COUNT(ride_id) as cnt
+# MAGIC FROM my_bike_view
+# MAGIC GROUP BY date_format(started_at,'EEEE')
+# MAGIC ORDER BY DAYOFWEEK(start_date)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We can observe that there are fewer rides on weekends compared to weekdays, supporting our hypothesis that the majority of users rely on our bikes for commuting to work rather than leisure activities. While there are mild fluctuations in usage throughout the week, the data remains significantly higher than weekend figures.
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
@@ -229,6 +122,11 @@ display(avg_trips_by_holiday_status)
 
 # COMMAND ----------
 
+# MAGIC %md 
+# MAGIC As we suspected that on a holiday we have lesser number of people using our bikes further confirming our hypothesis that the bikes at this station is being used primarily for commuting to work rather than leisure rides
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC Analyzing Weather data
 
@@ -246,9 +144,7 @@ merged_df.printSchema()
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col,round
-from pyspark.sql.functions import hour,date_trunc
-from pyspark.sql.types import StringType, FloatType
+
 merged_df = merged_df.withColumn("temp", col("temp").cast(FloatType()))
 merged_df = merged_df.withColumn("rounded_temp", round(col("temp"), -1))
 display(merged_df.limit(5))
@@ -256,16 +152,17 @@ display(merged_df.limit(5))
 # COMMAND ----------
 
 
-temperature_hourly_trend = merged_df.groupBy("rounded_temp", hour("started_at").alias("hour")) \
-    .agg(count("ride_id").alias("num_trips")) \
-    .orderBy("rounded_temp", "hour")
+# temperature_hourly_trend = merged_df.groupBy("rounded_temp", hour("started_at").alias("hour")) \
+#     .agg(count("ride_id").alias("num_trips")) \
+#     .orderBy("rounded_temp", "hour")
 
-display(temperature_hourly_trend)
+# display(temperature_hourly_trend)
 
 
 # COMMAND ----------
 
-import pyspark.sql.functions as F
+# MAGIC %md
+# MAGIC We observed that the number of rides decreases significantly between midnight and early morning, specifically from 12 AM to 6 AM. A peak in rides can be seen during late afternoon to evening hours, with the highest volume occurring between 4 PM and 6 PM. This pattern reflects the typical working schedule of the general population.
 
 # COMMAND ----------
 
@@ -279,33 +176,43 @@ display(hourly_trend)
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC General trend as expected. As temperature Rises more people ride bicycles 
+# MAGIC As anticipated, there is a general trend that demonstrates an increase in bicycle ridership as temperatures rise. However, when temperatures become excessively high, there is a noticeable decline in the number of rides.
 
 # COMMAND ----------
 
-hourly_trend_viz = merged_df.groupBy(F.date_trunc("hour", F.col("started_at")).alias("hour"), F.col("visibility")) \
+# daily_trend = merged_df.groupBy(F.date_trunc("day", F.col("started_at")).alias("day"), F.col("rounded_temp")) \
+#     .agg(F.count("ride_id").alias("num_rides")) \
+#     .orderBy("day", "rounded_temp")
+
+# display(daily_trend)
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# daily_trend_cloud = merged_df.groupBy(F.date_trunc("day", F.col("started_at")).alias("day"), F.col("clouds")) \
+#     .agg(F.count("ride_id").alias("num_rides")) \
+#     .orderBy("day", "clouds")
+
+# display(daily_trend_cloud)
+
+
+# COMMAND ----------
+
+hourly_trend_dew = merged_df.groupBy(F.date_trunc("hour", F.col("started_at")).alias("hour"), F.col("dew_point")) \
     .agg(F.count("ride_id").alias("num_rides")) \
-    .orderBy("hour", "visibility")
+    .orderBy("hour", "dew_point")
 
-display(hourly_trend_viz)
-
-
-# COMMAND ----------
-
-hourly_trend_viz.select('visibility').distinct().collect()
-
-# COMMAND ----------
-
-daily_trend = merged_df.groupBy(F.date_trunc("day", F.col("started_at")).alias("day"), F.col("rounded_temp")) \
-    .agg(F.count("ride_id").alias("num_rides")) \
-    .orderBy("day", "rounded_temp")
-
-display(daily_trend)
+display(hourly_trend_dew)
 
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC We cannot discern a specific trend in relation to dew point, although there are instances where a decrease in bike rides coincides with a drop in dew point.
 
 # COMMAND ----------
 
